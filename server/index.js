1b350c2e871a4bc7b5868b983b08cfa39cce7ae4
Daniel Alecrim <98345886+danielalecrimofc@users.noreply.github.com>
2023-03-31 12:28:51 -0300

    Configurações de Conexão do Banco de Dados e do Servidor
    Coloquei alguns scripts para conexão e configuração do banco de dados e servidor  !

--- server/index.js
+++ server/index.js
@@ -1,10 +1,39 @@
+const sql = require("mssql");
 const express = require("express");
+
 const app = express();
+
+//Criando a variável para autenticação do banco de dados
+const config = {
+    user:'sa',
+    password:'danielsam1999',
+    server:'DESKTOP-RDICEJ4',
+    database:'serv_mais',
+    options: {
+        trustServerCertificate: true
+      }
+}
+
+//Criando a variável para a conexão
+const pool = new sql.ConnectionPool(config);
+
+//Realizando a conexão com o banco de dados
+pool.connect(err => {
+    if(err){
+        console.error('Erro ao conectar no banco de dados:', err);
+    }else{
+        console.log('Conexão com o banco de dados estabelecida com sucesso!')
+        app.listen(3001, () => {
+        console.log('Aplicativo rodando na porta 3001.')
+        })
+    }
+})
 
 app.get("/",(req, res) =>{
     res.send("Hello World!");
 })
 
-app.listen(3001, () =>{
+//Função para saber se o aplicativo está rodando/escutando
+/*app.listen(3001, () =>{
     console.log("Rodando na porta 3001!");
-})
+})*/
